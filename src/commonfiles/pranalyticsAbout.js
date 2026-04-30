f38df9e5cda1f70cde7eadd4756c349a75f1d8ac
akhilgandepalli <akhilgandepalli@gmail.com>
2026-04-30 14:21:57 +0530

    updated

--- src/commonfiles/pranalyticsAbout.js
+++ src/commonfiles/pranalyticsAbout.js
@@ -4,7 +4,7 @@ export const pranalyticsAbout = {
     "Pranalyticx is a comprehensive platform designed to provide insights into public relations activities and their impact on brand perception and media coverage." +
     " It offers tools for monitoring media mentions, analyzing sentiment, and measuring the effectiveness of PR campaigns.",
   email: "info@pranalyticx.com",
-  phone: "+27 (62) 924-8796",
+  phone: "+27 629248796",
   altPhone: "+27 731143345",
   address: "Western Cape, Cape Town, South Africa",
   timings: "Mon - Fri: 10AM - 6PM",
